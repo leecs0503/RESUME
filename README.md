913cd46bad673c4c4bb72c0ca280a75c5f18ed82
lee changsu <leecs0503@naver.com>
2023-12-23 14:45:13 +0900

    feat: 프로젝트 기본 세팅

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # RESUME
+resume 관리를 위한 nextjs 레포지토리입니다.
+
+# Dependency
+node=^18.17.0
---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  eslint: {
+    dirs: ['pages', 'components', 'lib', 'app', 'utils', 'lib', 'script']
+  },
+};
